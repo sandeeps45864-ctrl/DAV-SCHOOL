f44f2a442e89e41334e2281d68fd8d7996a2b462
Sandeep <sandeeps45864@gamil.com>
2026-05-24 23:20:09 +0530

    fixed admission save issue

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+window.addEventListener("scroll", () => {
+
+    const navbar = document.querySelector(".navbar");
+
+    if (window.scrollY > 50) {
+
+        navbar.style.background = "#061f4f";
+
+    } else {
+
+        navbar.style.background = "#0B3D91";
+    }
+});
+
+
+
+// ================= ADMISSION FORM =================
+
 const form = document.getElementById("admissionForm");
 
 form.addEventListener("submit", async (e) => {
@@ -15,8 +33,9 @@ form.addEventListener("submit", async (e) => {
         className: document.getElementById("className").value,
 
         message: document.getElementById("message").value
-
     };
+
+    console.log(formData);
 
     try {
 
@@ -33,6 +52,10 @@ form.addEventListener("submit", async (e) => {
             }
         );
 
+        const data = await response.json();
+
+        console.log(data);
+
         if (response.ok) {
 
             alert("Admission Form Submitted Successfully");
@@ -41,7 +64,7 @@ form.addEventListener("submit", async (e) => {
 
         } else {
 
-            alert("Failed To Submit Form");
+            alert(data.message || "Failed To Submit Form");
         }
 
     } catch (error) {
@@ -50,5 +73,42 @@ form.addEventListener("submit", async (e) => {
 
         alert("Server Error");
     }
-
 });
+
+
+
+// ================= LOAD GALLERY =================
+
+async function loadGallery() {
+
+    try {
+
+        const response = await fetch(
+            "https://signup-login-1-b57h.onrender.com/api/gallery"
+        );
+
+        const data = await response.json();
+
+        const galleryGrid =
+            document.getElementById("galleryGrid");
+
+        galleryGrid.innerHTML = "";
+
+        data.forEach((item) => {
+
+            galleryGrid.innerHTML += `
+
+            <img
+            src="https://signup-login-1-b57h.onrender.com/uploads/${item.image}"
+            >
+
+            `;
+        });
+
+    } catch (error) {
+
+        console.log(error);
+    }
+}
+
+loadGallery();
